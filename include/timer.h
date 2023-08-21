0611dd9781e0b2483b5d52e0527690195a31bd5c
Hyam <hyam191018@gmail.com>
2023-08-21 20:58:56 +0800

    update comment

--- include/timer.h
+++ include/timer.h
@@ -7,7 +7,6 @@ auto timer_start(void) {
 }
 
 void timer_end(auto& start) {
-
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
     cout << "Elapsed time: " << duration.count() << " us" << std::endl;
