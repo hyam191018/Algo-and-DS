a55c648d40bcbcc38f334712d31a9debf0dad90b
Hyam <hyam191018@gmail.com>
2023-08-27 13:45:19 +0800

    update tree compare

--- source/tree_test.cpp
+++ source/tree_test.cpp
@@ -1,5 +1,6 @@
 #include "../include/list.h"
 #include "../include/rbtree.h"
+#include "../include/timer.h"
 #include "../include/tree.h"
 #include <cstdlib>
 #include <ctime>
@@ -12,23 +13,103 @@ int main() {
     BinarySearchTree bst;
     RedBlackTree rbt;
     SinglyLinkedList list;
-    const int numOperations = 10000; // Number of operations (insert, search, remove)
+    const int numOperations = 10000;
 
-    // Insertion phase
+    cout << "# Testing insert" << endl;
+    auto time = timerStart();
     for (int i = 0; i < numOperations; ++i) {
-        int num = rand() % numOperations; // Generate a random number
-        avl.insert(num);
-        bst.insert(num);
-        rbt.insert(num);
-        list.insert(num);
+        list.insert(i);
     }
-    // Removal phase
-    for (int i = 0; i < numOperations / 2; ++i) {
-        int num = rand() % numOperations; // Generate a random number
-        avl.remove(num);
-        bst.remove(num);
-        rbt.remove(num);
+    cout << "Linked list ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        bst.insert(i);
+    }
+    cout << "BST ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        avl.insert(i);
+    }
+    cout << "AVL Tree ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        rbt.insert(num);
+    }
+    cout << "RB Tree ";
+    timerEnd(time);
+
+    cout << "# Testing search" << endl;
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        list.search(num);
+    }
+    cout << "Linked list ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        bst.search(num);
+    }
+    cout << "BST ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        avl.search(num);
+    }
+    cout << "AVL Tree ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        rbt.search(num);
+    }
+    cout << "RB Tree ";
+    timerEnd(time);
+
+    cout << "# Testing remove" << endl;
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
         list.remove(num);
     }
+    cout << "Linked list ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        bst.remove(num);
+    }
+    cout << "BST ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        avl.remove(num);
+    }
+    cout << "AVL Tree ";
+    timerEnd(time);
+
+    time = timerStart();
+    for (int i = 0; i < numOperations; ++i) {
+        int num = rand() % numOperations;
+        rbt.remove(num);
+    }
+    cout << "RB Tree ";
+    timerEnd(time);
+
     return 0;
 }
